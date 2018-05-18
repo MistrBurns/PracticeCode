6e4cb6fdf4bda95f2ad29b9b90b2cf1af65628cb
MistrBurns <christianbernoegger@gmail.com>
2018-05-18 17:53:59 +0200

    finished module 1

---
+++ NetCore/NetCore/IGreeter.cs
@@ -0,0 +1,15 @@
+﻿namespace NetCore
+{
+    public interface IGreeter
+    {
+        string GetMessage();
+    }
+
+    public class Greeter : IGreeter
+    {
+        public string GetMessage()
+        {
+            return "Greetings";
+        }
+    }
+}
--- NetCore/NetCore/Startup.cs
+++ NetCore/NetCore/Startup.cs
@@ -35,4 +35,3 @@ namespace NetCore
         }
     }
 }
-6
